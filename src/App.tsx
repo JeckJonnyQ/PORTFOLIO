682a8fec5a38ee56b79ab4597ea3e9d9e20802b3
Reut Vladislav <reut-2011@mail.ru>
2024-07-29 21:34:03 +0300

    init: start project, init vite-template, install deps

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 style={{ color: "white" }}>
+      Start PortfolioStart PortfolioStart PortfolioStart PortfolioStart
+      PortfolioStart PortfolioStart PortfolioStart PortfolioStart PortfolioStart
+      PortfolioStart PortfolioStart PortfolioStart PortfolioStart PortfolioStart
+      PortfolioStart PortfolioStart PortfolioStart PortfolioStart PortfolioStart
+      PortfolioStart PortfolioStart Portfolio
+    </h1>
+  );
+}
+
+export default App;
