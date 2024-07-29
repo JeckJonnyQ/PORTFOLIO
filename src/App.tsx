f9bea4f91c5cbd246f705407824745d4c0270e5a
Reut Vladislav <reut-2011@mail.ru>
2024-07-29 23:37:34 +0300

    refactor: testing commit for husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 
 function App() {
+  const asd: any = 'asd';
+
   return (
     <h1 style={{ color: 'white' }}>
       Start PortfolioStart PortfolioStart PortfolioStart PortfolioStart PortfolioStart PortfolioStart PortfolioStart
